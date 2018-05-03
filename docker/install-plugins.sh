3f392bd949b56943329669ffada6aeb0d2d50eed
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-05-03 10:59:36 +0200

    Fix npm install for plugins

---
+++ docker/install-plugins.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+working_dir="/var/app"
+plugins_dir="plugins/enabled"
+
+cd "$working_dir"
+
+# npm install plugins
+for target in ${plugins_dir}/* ; do
+  if [ -d "$target" ]; then
+    echo 'Installing dependencies for ' $(basename "$target")
+    cd "$target"
+    npm install --unsafe
+    cd "$working_dir"
+  fi
+done
--- docker/run-dev.sh
+++ docker/run-dev.sh
@@ -8,6 +8,8 @@ log () {
 
 elastic_host=${kuzzle_services__db__client__host:-http://elasticsearch:9200}
 
+/install-plugins.sh
+
 log "Waiting for elasticsearch"
 while ! curl -f -s -o /dev/null "$elastic_host"
 do
